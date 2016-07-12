3f6b166da719d18f6b7fd566d8f1bf67c4fca5c8
Harry Torry <harry@brightmachine.co.uk>
2016-07-12 13:51:49 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Logging slack messages to a DB
+
+* Fill in the DB fields and give it a slack token (can get it from here - https://api.slack.com/methods/rtm.start/test)
+* Run `php server.php`
+* Comment out `migrationUp();`
+* Watch as your DB fills up!
---
+++ server.php
@@ -0,0 +1,73 @@
+<?php
+    require 'vendor/autoload.php';
+    use Illuminate\Database\Capsule\Manager as Capsule;
+    use Illuminate\Database\Eloquent\Model as Model;
+
+    class Message extends Model {
+        protected $table = 'messages';
+        protected $fillable = ['reply_to', 'type', 'channel', 'user', 'text', 'ts'];
+    }
+
+    setupDatabase();
+    migrationUp();
+//    migrationDown();
+
+    $loop = React\EventLoop\Factory::create();
+
+    $client = new Slack\RealTimeClient($loop);
+    $client->setToken('');
+
+    $client->on('message', function (Slack\Payload $data) use ($client) {
+        echo sprintf(
+            'Message~ %s: %s%s',
+            $data['user'],
+            $data['text'],
+            PHP_EOL
+        );
+
+        (new Message($data->getData()))->save();
+    });
+
+    $client->connect()->then(function () {
+        echo "Connected!\n";
+    });
+
+    $loop->run();
+
+    function setupDatabase() {
+        $capsule = new Capsule();
+
+        $capsule->addConnection([
+            'driver'    => 'mysql',
+            'host'      => '192.168.33.7',
+            'database'  => 'personal_slack',
+            'username'  => 'root',
+            'password'  => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]);
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+    }
+
+    function migrationUp() {
+        Capsule::schema()->create('messages', function($table)
+        {
+            $table->increments('id');
+            $table->integer('reply_to');
+            $table->string('type');
+            $table->string('channel');
+            $table->string('user');
+            $table->string('text', 4096);
+            $table->string('ts');
+            $table->timestamps();
+        });
+        die('Tables created, comment out `migrationUp();`' . PHP_EOL);
+    }
+
+    function migrationDown() {
+        Capsule::schema()->drop('messages');
+        die('Tables deleted, comment out `migrationDown();`' . PHP_EOL);
+    }
+
